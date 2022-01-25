281d47ff6657062011da4516bd1679beafc19a3a
Addison Cahill <acahill@chroniccareiq.com>
2022-01-25 14:29:42 -0500

    commit on master

---
+++ Pages/SiteFooter.js
@@ -0,0 +1,11 @@
+class SiteFooter extends HTMLElement{
+    constructor() {
+        super();
+
+        this.innerHTML = 
+        `
+        `;
+    }
+}
+
+windows.customElements.define("site-footer", SiteFooter);
