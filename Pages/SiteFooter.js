44ba4de821ded3b1006ba4dcfe5101be1d2e3ac6
Addison Cahill <acahill@chroniccareiq.com>
2022-01-25 22:40:11 -0500

    Updated sitefooter element and added to pages.

--- Pages/SiteFooter.js
+++ Pages/SiteFooter.js
@@ -3,9 +3,21 @@ class SiteFooter extends HTMLElement{
         super();
 
         this.innerHTML = 
-        `
+        ` <div class="container-fluid" id="navigation-bar">
+        <nav class="navbar navbar-expand-sm">
+            <div class="container-fluid">
+                <ul  class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="#">BLOG</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">TERMS OF USE</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">PRIVACY POLICY</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">COOKIE POLICY</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">CONTACT US</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
         `;
     }
 }
 
-windows.customElements.define("site-footer", SiteFooter);
+window.customElements.define("site-footer", SiteFooter);
