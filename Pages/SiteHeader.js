d14001ceaf48547ed07ef981a7f82e5590c062e7
Addison Cahill <acahill@chroniccareiq.com>
2022-01-25 22:31:18 -0500

    Updated header element with new code.

--- Pages/SiteHeader.js
+++ Pages/SiteHeader.js
@@ -3,33 +3,31 @@ class SiteHeader extends HTMLElement{
         super();
 
         this.innerHTML =  
-        `<header>
-        <div style="text-align: center; background-color: lightblue;">
-            <h style="font-size: 40px">Welcome to <b>28:19!</b></h>
-            <p><em>A space for encouraging and giving generously to those looking to reflect Christ, love others, and share the gospel!</em></p>
-        
-            <nav class="navbar navbar-inverse">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <a class="navbar-brand" href="index.html">28:19</a>
-                    </div>
-                    <ul class="nav navbar-nav">
-                        <li>About Us</li>
-                        <li><a href="../Pages/For Givers.html">For Givers</a></li>
-                        <li><a href="../Pages/For Missionaries.html">For Missionaries</a></li>
-                    </ul>
-                        
-
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="Sign In.html">Login</li>
-                    </ul>
-                    <!-- Put in a clickable icon for loging in, signing up, our help. -->
-                    <img style="display: flex;" src=../Images/No User Photo.png>
-                   
-                </div>
-            <nav>
-        </div>
-        </header>`;
+        ` <div class="container-fluid" id="navigation-bar">       
+        <nav class="navbar navbar-expand-sm sticky-top">
+            <div class="container-fluid">
+                <!--Code for navbar brand-->
+                <a class=navbar-brand href="index.html">
+                    <img src="../Images/2819 Logo.png">Welcome!
+                </a>
+                <ul class="navbar-nav">
+                    <!--Code for navbar dropdown-->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">About Us</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="About Us Pages/Meet Our Team.html">Our Team</a></li>
+                            <li><a class="dropdown-item" href="About Us Pages/Our Story.html">Our Story</a></li>
+                        </ul> 
+                    </li>
+                    <li class="nav-link"><a href="For Givers.html">For Givers</a></li>
+                    <li class="nav-link"><a href="For Missionaries.html">For Missionaries</a></li>
+                    <li class="nav-link"><a href="#"></a></li>
+                </ul>        
+                <button class="btn btn-primary" type="button">Sign In</button>
+                <button class="btn btn-primary" type="button">Register!</button>
+            </div>
+        </nav>
+    </div>`;
     }
 
 
