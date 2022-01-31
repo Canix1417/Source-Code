1a729a89bdfcc569b5116494e34d6fab1502f31a
Addison Cahill <acahill@chroniccareiq.com>
2022-01-31 10:11:24 -0500

    #13 Added image, styling, etc. Made nav topsticky.

--- Pages/SiteHeader.js
+++ Pages/SiteHeader.js
@@ -4,16 +4,15 @@ class SiteHeader extends HTMLElement{
 
         this.innerHTML =  
         ` <div class="container-fluid" id="navigation-bar">       
-        <nav class="navbar navbar-expand-sm stikcy-top">
-            <div class="container-fluid">
-                <!--Code for navbar brand-->
-                <a class=navbar-brand href="index.html">
-                    <img src="../Images/2819 Logo.png">
-                </a>
-                <ul class="navbar-nav">
-                    <!--Code for navbar dropdown
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">About Us</a>
+                <nav class="navbar navbar-expand-sm fixed-top trans-bg">
+                    <div class="container-fluid">
+                    <a class=navbar-brand href="index.html">
+                        <img src="../Images/2819 Logo.png">
+                    </a>
+                    <ul class="navbar-nav">
+                        <!--Code for navbar dropdown
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">About Us</a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="About Us Pages/Meet Our Team.html">Our Team</a></li>
                             <li><a class="dropdown-item" href="About Us Pages/Our Story.html">Our Story</a></li>
