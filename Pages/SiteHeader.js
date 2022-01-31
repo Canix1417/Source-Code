11e3e56dc7a3de0dde433cf710a6798a3d6aa099
Addison Cahill <acahill@chroniccareiq.com>
2022-01-31 14:02:36 -0500

    #12 Updated navbar elements & styling.

--- Pages/SiteHeader.js
+++ Pages/SiteHeader.js
@@ -3,38 +3,39 @@ class SiteHeader extends HTMLElement{
         super();
 
         this.innerHTML =  
-        ` <div class="container-fluid" id="navigation-bar">       
-                <nav class="navbar navbar-expand-sm fixed-top trans-bg">
-                    <div class="container-fluid">
+        `
+        <div class="container-fluid" id="navigation-bar">       
+            <nav class="navbar navbar-expand-sm fixed-top trans-bg">
+                <div class="container-fluid">
                     <a class=navbar-brand href="index.html">
-                        <img src="../Images/2819 Logo.png">
+                        <img id="logo" src="../Images/2819 Logo.png">
                     </a>
                     <ul class="navbar-nav">
-                        <!--Code for navbar dropdown
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">About Us</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="About Us Pages/Meet Our Team.html">Our Team</a></li>
-                            <li><a class="dropdown-item" href="About Us Pages/Our Story.html">Our Story</a></li>
-                        </ul> 
-                    </li>-->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="" data-bs-toggle="dropdown">For Givers</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="For Givers.html">Getting Started</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" href="For Missionaries.html">For Missionaries</a>
-                    </li>
-                </ul>        
-                <div>
-                    <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#signInModal">Sign In</button>
-                    <a href="Register Pages/Register.html"><button class="btn btn-primary" type="button">Register</button></a>
+                        <li id="nav-dropdown1" class="nav-item dropdown">
+                            <a class="nav-link" href="" data-bs-toggle="dropdown">For Givers</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="For Givers.html">Getting Started</a></li>
+                                <li><a class="dropdown-item" href="">Giving Opportunities</a></li>
+                                <li><a class="dropdown-item" href="">FAQs</a></li>
+                            </ul>
+                        </li>
+                        <li id="nav-dropdown2" class="nav-item dropdown">
+                            <a class="nav-link" href="" data-bs-toggle="dropdown">For Missionaries</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="For Missionaries.html">Getting Started</a></li>
+                                <li><a class="dropdown-item" href="">Encouragement</a></li>
+                                <li><a class="dropdown-item" href="">Fundraising Tips</a></li>
+                                <li><a class="dropdown-item" href="">FAQs</a></li>
+                            </ul>
+                        </li>
+                    </ul>        
+                    <div>
+                        <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#signInModal">Sign In</button>
+                        <a href="Register Pages/Register.html"><button class="btn btn-primary" type="button">Register</button></a>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    </div>`;
+            </nav>
+        </div>`;
     }
 
 
